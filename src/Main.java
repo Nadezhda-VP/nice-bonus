4e9ca9649d05c0a36a183d897b6d5e340b896d57
Nady <nadezhda.vp.qa@gmail.com>
2023-06-13 18:29:42 +0300

    Main changed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int initialAccount = 500;
+        int depositAmount = 200;
+        int bonus;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        int balance = initialAccount + depositAmount + bonus;
+        System.out.println("Баланс: " + balance + " руб. Бонус: " + bonus + " руб.");
     }
 }
